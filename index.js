46b881b99f895dc5e30125d0952802cd69ddae68
Apex <arpana036@gmail.com>
2025-01-15 02:48:26 +0530

    description

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/",(req,res)=>{
-    res.send("hiii")
+    res.send("hiii this is the backend for weather app")
 })
 
 app.use('/signup',require("./authorization/signup"))
