dd50e72659c554c4ce420556686d078340768699
Apex <arpana036@gmail.com>
2025-09-10 23:42:54 +0530

    removed .git from repository

--- authentication/check_login.js
+++ authentication/check_login.js
@@ -1,11 +1,11 @@
 const app=require("express")
 const Router=app.Router()
 const jwt=require("jsonwebtoken")
+require("dotenv").config()
 
 Router.post('/',async (req,res)=>{
     const token=req.headers.authorization.split(' ')[1]
-    jwt.verify(token,`weatheraxos`,(error,decoded)=>{
-        console.log('wef')
+    jwt.verify(token,process.env.JWT_PRIVATE_KEY,(error,decoded)=>{
         if(error){
             
             return res.status(401).json({success:false});
